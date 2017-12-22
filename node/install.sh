41dccc8451a6f13429df9530bb387ead242ccb21
Zack Tollman <tollmanz@gmail.com>
2017-12-22 12:47:08 -0600

    Update installer to latest version

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-SCRIPT="https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh"
+SCRIPT="https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh"
 DIR="${HOME}/.nvm"
 
 install_nvm() {
